ffea7f94be4ce58cda69edb110e9d14c967bea03
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-05-02 13:47:02 +0900

    BUILD: Project setting

---
+++ src/main/java/com/capstone/gooddayserver/GoodDayServerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.gooddayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodDayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodDayServerApplication.class, args);
+	}
+
+}
