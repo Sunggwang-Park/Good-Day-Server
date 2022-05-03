6d86b1dbe72fff75f5c6fdd547f89549aa2a0361
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-05-03 16:01:32 +0900

    FEATURE: 유저 엔티티 생성

--- src/main/java/com/capstone/gooddayserver/domain/user/entity/User.java
+++ src/main/java/com/capstone/gooddayserver/domain/user/entity/User.java
@@ -1,9 +1,19 @@
 package com.capstone.gooddayserver.domain.user.entity;
 
 
+import com.capstone.gooddayserver.configure.entity.BaseTimeEntity;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class User extends BaseTimeEntity {
 
-public class User {
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
 
 }
