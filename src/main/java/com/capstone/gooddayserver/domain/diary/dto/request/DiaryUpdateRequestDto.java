c10091f9b4b90a9900ccf5dbf0f52eb2a058c479
hwangjiwon98 <hjw9879@naver.com>
2022-05-20 09:47:00 +0900

    CHORE: DiaryUpdateRequestDto 수정

--- src/main/java/com/capstone/gooddayserver/domain/diary/dto/request/DiaryUpdateRequestDto.java
+++ src/main/java/com/capstone/gooddayserver/domain/diary/dto/request/DiaryUpdateRequestDto.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Builder
-public class DiaryCreateRequestDto {
+public class DiaryUpdateRequestDto {
 
     private Long userId;
     private String title;
