45884a4c2e73b59eef096f00acf62760bfc0a409
hwangjiwon98 <hjw9879@naver.com>
2022-05-22 18:21:01 +0900

    TEST: 일기장 수정 테스트 코드 수정

--- src/test/java/com/capstone/gooddayserver/domain/diary/service/DiaryServiceTest.java
+++ src/test/java/com/capstone/gooddayserver/domain/diary/service/DiaryServiceTest.java
@@ -1,6 +1,7 @@
 package com.capstone.gooddayserver.domain.diary.service;
 
 import com.capstone.gooddayserver.domain.diary.dto.request.DiaryCreateRequestDto;
+import com.capstone.gooddayserver.domain.diary.dto.request.DiaryUpdateRequestDto;
 import com.capstone.gooddayserver.domain.diary.entity.Diary;
 import com.capstone.gooddayserver.domain.diary.repository.DiaryRepository;
 import com.capstone.gooddayserver.domain.user.entity.User;
@@ -28,30 +29,37 @@ class DiaryServiceTest {
 
     @Test
     @Transactional
-    public void 일기_저장_및_조회() throws Exception {
+    public void 일기_수정() throws Exception {
         //given
-
         User user = User.builder()
                 .nickname("성광")
                 .mbti("INTJ")
                 .sleepTime(LocalTime.parse("22:00"))
                 .wakeUpTime(LocalTime.parse("09:00"))
                 .build();
-
         userRepository.save(user);
 
-        DiaryCreateRequestDto dto = DiaryCreateRequestDto.builder()
+        Diary diary = Diary.builder()
                 .title("오늘의 일기")
                 .content("기분이 좋다")
+                .date(LocalDate.parse("2022-05-12"))
                 .build();
+        diaryRepository.save(diary);
+
+        DiaryUpdateRequestDto dto = DiaryUpdateRequestDto.builder()
+                .userId(user.getId())
+                .title("바뀐 제목")
+                .build();
+
+
         //when
-        diaryService.createDiary(dto, user);
+        diaryService.updateDiaryInfo(diary.getId(), dto, user);
 
 
 
         //then
-        Diary diary = diaryRepository.findDiaryByUser(user);
-        Assertions.assertThat(diary.getTitle()).isEqualTo("오늘의 일기");
+        Diary diary1 = diaryRepository.findDiaryByUser(user);
+        Assertions.assertThat(diary1.getTitle()).isEqualTo(dto.getTitle());
 
 
 
