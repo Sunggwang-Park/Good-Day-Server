cccb03f317672ca5dff08e95e90c972a7692f644
hwangjiwon98 <hjw9879@naver.com>
2022-05-28 19:27:57 +0900

    FIX : PatchMapping add @reponsebody

--- src/main/java/com/capstone/gooddayserver/controller/DiaryController.java
+++ src/main/java/com/capstone/gooddayserver/controller/DiaryController.java
@@ -3,16 +3,15 @@ package com.capstone.gooddayserver.controller;
 
 import com.capstone.gooddayserver.domain.diary.dto.request.DiaryCreateRequestDto;
 import com.capstone.gooddayserver.domain.diary.dto.request.DiaryUpdateRequestDto;
+import com.capstone.gooddayserver.domain.diary.dto.response.DiaryCreateResponseDto;
 import com.capstone.gooddayserver.domain.diary.dto.response.DiaryInfoResponseDto;
+import com.capstone.gooddayserver.domain.diary.dto.response.DiaryUpdateResponseDto;
+import com.capstone.gooddayserver.domain.diary.entity.Diary;
 import com.capstone.gooddayserver.domain.diary.service.DiaryService;
 import com.capstone.gooddayserver.domain.user.dto.request.UserIdRequestDto;
 import com.capstone.gooddayserver.domain.user.entity.User;
-import com.capstone.gooddayserver.domain.user.repository.UserRepository;
 import com.capstone.gooddayserver.domain.user.service.UserService;
-import com.capstone.gooddayserver.exception.CustomException;
-import com.capstone.gooddayserver.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -26,25 +25,71 @@ public class DiaryController {
     private final UserService userService;
 
 
+    // 일기 작성
     @PostMapping("join")
-    public void createDiary(@RequestBody DiaryCreateRequestDto dto) {
+    @ResponseBody
+    public DiaryCreateResponseDto createDiary(@RequestBody DiaryCreateRequestDto dto) {
         //유저 존재 유무 판단
         System.out.println("dto = " + dto);
         User user = userService.getUser(dto.getUserId());
 
-        diaryService.createDiary(dto, user);
+        Diary diary = diaryService.createDiary(dto, user);
+
+        DiaryCreateRequestDto diaryInfoInDB = DiaryCreateRequestDto.builder()
+                .userId(user.getId())
+                .title(diary.getTitle())
+                .content(diary.getContent())
+                .imgUrl(diary.getImgUrl())
+                .date(diary.getDate())
+                .build();
+
+        DiaryCreateResponseDto responseDto = DiaryCreateResponseDto.builder()
+                .msg("success")
+                .userId(user.getId())
+                .data(diaryInfoInDB)
+                .build();
+
+        System.out.println("responseDto = " + responseDto);
+
+        return responseDto;
+
     }
 
+
+
+    // 일기 조회
     @GetMapping("{diaryId}")
     public DiaryInfoResponseDto getDiaryInfo(@PathVariable("diaryId") Long diaryId, @RequestBody UserIdRequestDto dto) {
+
         User user = userService.getUser(dto.getUserId());
         return diaryService.getDiary(diaryId, user);
+
     }
 
+
+
+    // 일기 수정
     @PatchMapping("{diaryId}")
-    public void updateDiaryInfo(@PathVariable("diaryId") Long diaryId, @RequestBody DiaryUpdateRequestDto dto) {
-        User user = userService.getUser(dto.getUserId());
-        diaryService.updateDiaryInfo(diaryId, dto, user);
+    @ResponseBody
+    public DiaryUpdateResponseDto updateDiaryInfo(@PathVariable("diaryId") Long diaryId, @RequestBody DiaryUpdateRequestDto dto) {
+
         System.out.println("dto = " + dto);
+        User user = userService.getUser(dto.getUserId());
+
+        diaryService.updateDiaryInfo(diaryId, dto, user);
+
+        DiaryInfoResponseDto diaryInfo = diaryService.getDiary(diaryId, user);
+
+        DiaryUpdateResponseDto responseDto = DiaryUpdateResponseDto.builder()
+                .msg("success")
+                .userId(user.getId())
+                .data(diaryInfo)
+                .build();
+
+        System.out.println("responseDto = " + responseDto);
+
+        return responseDto;
+
     }
+
 }
--- src/main/java/com/capstone/gooddayserver/controller/UserController.java
+++ src/main/java/com/capstone/gooddayserver/controller/UserController.java
@@ -1,26 +1,25 @@
 package com.capstone.gooddayserver.controller;
 
-
 import com.capstone.gooddayserver.domain.user.dto.request.UserIdRequestDto;
 import com.capstone.gooddayserver.domain.user.dto.request.UserJoinRequestDto;
 import com.capstone.gooddayserver.domain.user.dto.request.UserUpdateRequestDto;
 import com.capstone.gooddayserver.domain.user.dto.response.UserInfoResponseDto;
 import com.capstone.gooddayserver.domain.user.dto.response.UserJoinResponseDto;
+import com.capstone.gooddayserver.domain.user.dto.response.UserUpdateResponseDto;
 import com.capstone.gooddayserver.domain.user.entity.User;
-import com.capstone.gooddayserver.domain.user.repository.UserRepository;
 import com.capstone.gooddayserver.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
-
 public class UserController {
 
     private final UserService userService;
 
+
+    // 회원 가입
     @PostMapping("join")
     @ResponseBody
     public UserJoinResponseDto join(@RequestBody UserJoinRequestDto dto) {
@@ -41,17 +40,41 @@ public class UserController {
 
 
         return responseDto;
+
     }
 
+
+
+    // 회원 정보 조회
     @GetMapping("my")
     public UserInfoResponseDto getUserInfo(@RequestBody UserIdRequestDto dto) {
+
         System.out.println("UserController.getUserInfo");
         return userService.getUserInfo(dto.getUserId());
+
     }
 
+
+
+    // 회원 정보 수정
     @PatchMapping("update")
-    public void updateUserInfo(@RequestBody UserUpdateRequestDto dto) {
+    @ResponseBody
+    public UserUpdateResponseDto updateUserInfo(@RequestBody UserUpdateRequestDto dto) {
+
         userService.updateInfo(dto);
         System.out.println("dto = " + dto);
+
+        UserInfoResponseDto userInfo = userService.getUserInfo(dto.getUserId());
+
+        UserUpdateResponseDto responseDto = UserUpdateResponseDto.builder()
+                .msg("success")
+                .userId(dto.getUserId())
+                .data(userInfo)
+                .build();
+
+        System.out.println("responseDto = " + responseDto);
+
+        return responseDto;
+
     }
 }
